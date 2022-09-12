e73be718ee6562132a2da608fa55bcb02a13f3d8
cuong.le-hung [BV] <cuong.le-hung@banvien.com.vn>
2022-09-13 04:16:43 +0700

    mutex.c added

--- learn-c/00-multithread/B-sync-thread/mutex.c
+++ learn-c/00-multithread/B-sync-thread/mutex.c
@@ -5,18 +5,29 @@
 #include <unistd.h>
 
 #define MAX_THREAD 5
+#define USE_MUTEX 1
+
 pthread_t tid[MAX_THREAD];
+#if USE_MUTEX
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+#endif
 
 /* Global variablies */
 int counter;
 
 void *threadFunc(void *argv)
 {
+#if USE_MUTEX
+	pthread_mutex_lock(&mutex);
+#endif
 	counter += 1;
 	printf("Thread %d has started\n", counter);
 
 	sleep(1);
 	printf("Thread %d has finished\n", counter);
+#if USE_MUTEX
+	pthread_mutex_unlock(&mutex);
+#endif
 
 	pthread_exit(NULL);
 }
@@ -33,12 +44,19 @@ int main(void)
 		{
 			printf("Thread [%d] created error\n", i);
 		}
-
+#if 0 // comment out this code to test sync thread using mutex
 		if (iret == 0)
 		{
 			pthread_join(tid[i], NULL);
 		}
+#endif
 	}
+#if 1 // un-comment out this code to test sync thread using mutex
+	for (i = 0; i < MAX_THREAD; i++)
+	{
+		pthread_join(tid[i], NULL);
+	}
+#endif
 
 	return 0;
 }
