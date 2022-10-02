d913babf7010b3b18a141167beb4ac280b336a1b
cuong.le-hung [BV] <cuong.le-hung@banvien.com.vn>
2022-10-02 23:33:34 +0700

    re-construct vim-config

--- vim-config/copy.sh
+++ vim-config/copy.sh
@@ -1,5 +1,18 @@
-echo "Copying files to ~/.config/nvim"
-cd config/nvim
-cp init.vim ~/.config/nvim/
-cp ginit.vim ~/.config/nvim/
-cp coc-settings.json ~/.config/nvim/
+#!/bin/bash
+SRC=./nvim
+DES=~/.config/nvim
+
+if [ ! -d "${DES}/settings" ]
+then
+	echo "*** ${DES} not exist. Creating ..."
+	mkdir -p ${DES}/settings
+else
+	echo "*** Removing old files"
+	rm ${DES}/*.vim
+	rm ${DES}/*.json
+	rm ${DES}/settings/*.vim
+fi
+
+echo "*** Copy files to ~/.config/nvim"
+cp -R ${SRC}/* ${DES}/
+tree ${DES} -L 2
---
+++ vim-config/nvim/settings/nerdtree.vim
@@ -0,0 +1,5 @@
+" F5 to toogle
+nnoremap <F5> :NERDTreeToggle<CR>
+
+" Highlight current file
+let g:nerdtree_sync_cursorline = 1 
